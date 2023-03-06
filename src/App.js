89015247be58d1ad0aca90d450f2d449866a40f4
Jaime D. Rodriguez <118196254+JDRGit@users.noreply.github.com>
2023-03-06 15:27:37 -0500

    css for SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import React, { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import ImageList from "./components/ImageList";
 import searchImages from "./api";
 
 function App() {
-  const handleSubmit = (term) => {
-    const result = searchImages(term);
-    console.log(result);
+  const [images, setImages] = useState([]);
+
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
+    setImages(result);
   };
 
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,16 +1,20 @@
 import axios from "axios";
 
+const clientId = "vBDfLG26ijbXWgccEA6aYzhJmqhG17Q17EIQFVu5xG0";
+const apiUrl = "https://api.unsplash.com/search/photos";
+
 const searchImages = async (term) => {
-  const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+  const response = await axios.get(apiUrl, {
     headers: {
-      Authorization: "Client-ID vBDfLG26ijbXWgccEA6aYzhJmqhG17Q17EIQFVu5xG0",
+      Authorization: `Client-ID ${clientId}`,
     },
     params: {
-      query: {term},
+      query: term,
     },
   });
-
+  console.log(response.data);
   return response.data.results;
 };
 
 export default searchImages;
+
